8abfbad76e1a3365024004bf6793e4bad3e0a87a
wudi <acgrid@gmail.com>
2021-05-05 15:59:06 +0800

    Refactor: replace with lambda expression

--- app/src/main/java/com/example/myfirstapp/ScanActivity.java
+++ app/src/main/java/com/example/myfirstapp/ScanActivity.java
@@ -1,11 +1,10 @@
 package com.example.myfirstapp;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.OptIn;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.CameraSelector;
-import androidx.camera.core.ExperimentalGetImage;
 import androidx.camera.core.ImageAnalysis;
-import androidx.camera.core.ImageProxy;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
@@ -60,30 +59,27 @@ public class ScanActivity extends AppCompatActivity {
                 .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
                 .build();
 
-        imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(this), new ImageAnalysis.Analyzer() {
-            @Override
-            @ExperimentalGetImage
-            public void analyze(@NonNull ImageProxy imageProxy) {
-                Image image = imageProxy.getImage();
-                if (image != null) {
-                    InputImage inputImage = InputImage.fromMediaImage(image, imageProxy.getImageInfo().getRotationDegrees());
-                    scanner.process(inputImage)
-                            .addOnCompleteListener(task -> {
-                                Log.i(TAG, task.toString());
-                                imageProxy.close();
-                            })
-                            .addOnSuccessListener(barCodes -> {
-                                for (Barcode barcode: barCodes) {
-                                    Log.d(TAG, barcode.getRawValue());
-                                    if (barcode.getValueType() == Barcode.TYPE_URL) {
-                                        // http://fpj.datarj.com/einv/fm?q=b249MDQyODIzNTYwMDA3MDA4NTE5MjAxJnNpPTVjZjU3Yjc4MGFhOTc2ZTVmNjlmYzVlYjFlM2NlMzIw
-                                        // on=042823560007008519201&si=5cf57b780aa976e5f69fc5eb1e3ce320
-                                        Toast.makeText(getApplicationContext(), barcode.getUrl().getUrl(), Toast.LENGTH_LONG).show();
-                                    }
+        imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(this), imageProxy -> {
+            @OptIn(markerClass = androidx.camera.core.ExperimentalGetImage.class)
+            Image image = imageProxy.getImage();
+            if (image != null) {
+                InputImage inputImage = InputImage.fromMediaImage(image, imageProxy.getImageInfo().getRotationDegrees());
+                scanner.process(inputImage)
+                        .addOnCompleteListener(task -> {
+                            Log.i(TAG, task.toString());
+                            imageProxy.close();
+                        })
+                        .addOnSuccessListener(barCodes -> {
+                            for (Barcode barcode: barCodes) {
+                                Log.d(TAG, barcode.getRawValue());
+                                if (barcode.getValueType() == Barcode.TYPE_URL) {
+                                    // http://fpj.datarj.com/einv/fm?q=b249MDQyODIzNTYwMDA3MDA4NTE5MjAxJnNpPTVjZjU3Yjc4MGFhOTc2ZTVmNjlmYzVlYjFlM2NlMzIw
+                                    // on=042823560007008519201&si=5cf57b780aa976e5f69fc5eb1e3ce320
+                                    Toast.makeText(getApplicationContext(), barcode.getUrl().getUrl(), Toast.LENGTH_LONG).show();
                                 }
-                            })
-                            .addOnFailureListener(e -> Log.e(TAG, "Decode barcode", e));
-                }
+                            }
+                        })
+                        .addOnFailureListener(e -> Log.e(TAG, "Decode barcode", e));
             }
         });
     }
